c3a5884ef0e91a87e56b40dafcf9a46e2dcd0359
Diego <diegoa.vallejor@uqvirtual.edu.co>
2025-02-05 12:28:07 -0500

    codigocomp

--- main.ino
+++ main.ino
@@ -1,8 +1,8 @@
-void setup (){
-
+void setup() {
+  Serial.begin(9600);        // Inicia la comunicación serial a 9600 baudios
+  Serial.println("Hola Mundo"); // Envía el mensaje "Hola Mundo" al monitor serial
 }
 
-void loop (){
-
-    
+void loop() {
+  // No se necesita código en el loop para este ejemplo
 }
